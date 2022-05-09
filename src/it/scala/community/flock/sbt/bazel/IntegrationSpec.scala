0f1eb1032e75c3ae5f7da513a949e5c2c106726c
Mark <mark@vectos.net>
2022-05-09 17:07:30 +0100

    Run on RBE

--- src/it/scala/community/flock/sbt/bazel/IntegrationSpec.scala
+++ src/it/scala/community/flock/sbt/bazel/IntegrationSpec.scala
@@ -3,26 +3,40 @@ package community.flock.sbt.bazel
 import buildinfo.BuildInfo
 
 import java.io.File
-import java.nio.file.{Files, OpenOption, Path, Paths, StandardOpenOption}
-import scala.io.Source
+import java.nio.file.{Files, Path, StandardOpenOption}
 import scala.sys.process.*
 
 class IntegrationSpec extends munit.FunSuite {
 
   def fileFromPath(path: String) = new File(getClass.getResource(path).getPath)
 
-  def run(path: File, cmd: String): Int = {
-    Process(cmd, path).run().exitValue()
+  def run(path: File, cmd: String): Int = Process(cmd, path).run().exitValue()
+
+  def writePluginFile(path: File): Path = {
+    val pluginsPath = path.toPath.resolve("project/plugins.sbt")
+    Files.createDirectories(pluginsPath.resolve("project"))
+    Files.write(pluginsPath, s"""addSbtPlugin("community.flock" % "sbt-bazel" % "${BuildInfo.version}")""".getBytes(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)
   }
 
-  def writePluginFile(path: File): Path =
-    Files.write(path.toPath.resolve("project/plugins.sbt"), s"""addSbtPlugin("community.flock" % "sbt-bazel" % "${BuildInfo.version}")""".getBytes(), StandardOpenOption.CREATE_NEW)
+  def writeBazelrc(path: File): Path = {
+    val bazelRcPath = path.toPath.resolve(".bazelrc")
+    val contents =
+      s"""build --bes_results_url=https://app.buildbuddy.io/invocation/
+        |build --bes_backend=grpcs://remote.buildbuddy.io
+        |build --remote_cache=grpcs://remote.buildbuddy.io
+        |build --remote_header=x-buildbuddy-api-key=${System.getenv("BUILDBUDDY_API_TOKEN")}
+        |build --remote_timeout=3600
+        |""".stripMargin
+
+    Files.write(bazelRcPath, contents.getBytes(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)
+  }
 
   test("should install sbt project") {
     val path = fileFromPath("/simple-zio-project")
     writePluginFile(path)
+    writeBazelrc(path)
     assertEquals(run(path, "sbt bazelInstall"), 0)
-    assertEquals(run(path, "bazelisk build //modules/api"), 0)
+    assertEquals(run(path, "bazel build //modules/api"), 0)
   }
 
 }
